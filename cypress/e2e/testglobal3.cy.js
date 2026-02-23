7b832d0c69f2f5bf86b5e5dad84a4d2470b7b3b2
Mari-fil <filimanyuk@gmail.com>
2026-02-23 02:21:13 +0200

    exclude page contacts

--- cypress/e2e/testglobal3.cy.js
+++ cypress/e2e/testglobal3.cy.js
@@ -3,7 +3,7 @@ const turkishPages = [
   "https://goit.global/tr/courses/",
   "https://goit.global/tr/reviews/",
   "https://goit.global/tr/articles/",
-  "https://goit.global/tr/contacts/",
+  // "https://goit.global/tr/contacts/",
   "https://goit.global/tr/terms-of-use/",
   "https://goit.global/tr/privacy-policy/",
   "https://goit.global/tr/newcomers/",
